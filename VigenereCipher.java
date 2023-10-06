e5458b95a15563c52c2d546eaada4b8cebf7faa0
kerabite <10791585+kerabite@users.noreply.github.com>
2023-10-05 23:02:18 -0500

    Moved helper functions to VigenereCipher

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -10,7 +10,7 @@ public class VigenereCipher {
     if (args.length != 3) {
       err.println("Incorrect number of parameters");
       System.exit(2);
-    }
+    } // if
 
     // get the instruction on whether or not to encode or decode
     String instruction = args[0].toLowerCase();
@@ -20,7 +20,7 @@ public class VigenereCipher {
         || instruction.equals("decode"))){
       err.println("Valid options are \"encode\" or \"decode\"");
       System.exit(1);
-    }
+    } // if
 
     // make normal output
     PrintWriter out = new PrintWriter(System.out, true);
@@ -30,15 +30,15 @@ public class VigenereCipher {
     String key = args[2];
 
     // encoding is shifting positively, decoding is shifting negatively
-    int fixN = 1;
-    if (instruction.equals("decode")) fixN = -1;
+    int direction = 1;
+    if (instruction.equals("decode")) direction = -1;
 
     // apply the instruction on the msg
-    out.println(xcode(msg, key, fixN));
-  }
+    out.println(xcode(msg, key, direction));
+  } // main
 
   // encode or decode a string
-  public static String xcode(String msg, String key, int fixN) {
+  public static String xcode(String msg, String key, int direction) {
     // avoiding division by zero
     if (key.length() == 0) return msg;
 
@@ -49,10 +49,24 @@ public class VigenereCipher {
     // loop over every letter
     for (int i = 0; i < toTransform.length; i++) {
       // cipher current letter
-      toTransform[i] = CaesarCipher.numToAlpha(CaesarCipher.rebaseChar(toTransform[i]) + (CaesarCipher.rebaseChar(keyArr[i % keyArr.length]) * fixN));
-    }
+      toTransform[i] = numToAlpha(rebaseChar(toTransform[i]) + (rebaseChar(keyArr[i % keyArr.length]) * direction));
+    } // for
 
     // return ciphered string
     return new String(toTransform); 
-  }
-}
+  } // xcode
+
+  // rebase character to 0-26 scale
+  public static int rebaseChar(char ch) {
+    return (int) ch - (int) 'a';
+  } // rebaseChar
+
+  // convert number to letter
+  public static char numToAlpha(int n) {
+    int modN = n % 26;
+    // dumb code because oracle has no clue what modulus is
+    if (modN < 0) modN = 26 + modN;
+
+    return (char) (modN + (int) 'a');
+  } // numToAlpha
+} // VignereCipher
